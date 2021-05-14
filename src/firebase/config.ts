6a1407ed3d8a1024ebeac1f16b35165bc4cc7fbd
santilp95 <santiaggola@hotmail.com>
2021-05-14 10:33:26 -0500

    1.configuracion inicial firebase

---
+++ src/firebase/config.ts
@@ -0,0 +1,16 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAs0oBzRTNYf3rfbqezb4aOLBSQNB3cnXY",
+    authDomain: "sql-demo-bef22.firebaseapp.com",
+    projectId: "sql-demo-bef22",
+    storageBucket: "sql-demo-bef22.appspot.com",
+    messagingSenderId: "984267367579",
+    appId: "1:984267367579:web:53be0557a152458e34523e",
+    measurementId: "G-SBCBE6Q2MY"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+
+  console.log('firebase')
