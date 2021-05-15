3f74b78829b71cdf9c7f37bcf7ce3158f9b98555
santilp95 <santiaggola@hotmail.com>
2021-05-15 17:12:31 -0500

    8.2 optener ultimo documento
    permtie optener el ultimo documento de firebase

--- src/index.ts
+++ src/index.ts
@@ -12,9 +12,23 @@ const btnNext = document.createElement('button');
 btnNext.innerText = 'Next Page';
 document.body.append(btnNext)
 
+//3 . saber cual es el ultimo documento que estamos buscando, por ejemplo hacer pagicnacion de 2 en 2
+// coger el ultimo registro luego los siguientes 2
+// tiene el mismo problema que cualquier bd y es que si alguien altera el orden puede cambiar
+let lastDocument = null
+
 btnNext.addEventListener('click',()=>{
-    console.log('click')
+    // console.log('click')
+    const query = usariosRef
+                    .orderBy('nombre');
+
+    query.limit(2).get().then(snap=>{
+
+        //con esto optengo el ultimo documento de firebase
+        lastDocument = snap.docs[snap.docs.length - 1]
+        retornaDocumentos(snap)
+    })
 })
 
-//simular el registro a penas carge la pagina
+//2.simular el registro a penas carge la pagina
 btnNext.click()
